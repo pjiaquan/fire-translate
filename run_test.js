19151c14347499803a01aa4816fe6a8d857db9f4
risc4 <risc4@example.com>
2026-08-20 21:28:45 +0800

    chore: remove temporary test file

--- run_test.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const backgroundCode = fs.readFileSync('background.js', 'utf8');
-const testCode = fs.readFileSync('tests.js', 'utf8');
-
-console.log(backgroundCode.includes('getBilingualLangName'));
